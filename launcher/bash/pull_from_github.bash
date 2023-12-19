b5e24b4c4da1075924444ea08c02be76122b43b0
septmacbot <129236685+septmacbot@users.noreply.github.com>
2023-12-19 12:46:25 -0500

    Update pull_from_github.bash

--- launcher/bash/pull_from_github.bash
+++ launcher/bash/pull_from_github.bash
@@ -1,7 +1,20 @@
 #! /bin/bash
 
 #   Ensure that SSH keys are configured correctly on your system before running
+# Need to create .git config
+# name=$(hostname)
+#email="septmacbot@gmail.com"
 
+#GITCONFIG_FILE="$HOME/.gitconfig"
+
+# Create or overwrite .gitconfig
+#echo "[user]" > "$GITCONFIG_FILE"
+#echo "    name = $COMPUTER_NAME" >> "$GITCONFIG_FILE"
+#echo "    email = $EMAIL" >> "$GITCONFIG_FILE"
+# Display success message
+echo "Git configuration set successfully."
+
+# this is for pulling
 cd ~/macbot
 git add .
 git commit -m "ignored"
