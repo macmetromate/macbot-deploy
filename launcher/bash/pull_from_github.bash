5fac2bc5ceb8bb64ecdaf7a776bab0be8acce819
septmacbot <129236685+septmacbot@users.noreply.github.com>
2023-12-20 23:27:21 -0500

    Update pull_from_github.bash

--- launcher/bash/pull_from_github.bash
+++ launcher/bash/pull_from_github.bash
@@ -1,18 +1,18 @@
 #! /bin/bash
 
 #   Ensure that SSH keys are configured correctly on your system before running
-# Need to create .git config
-# name=$(hostname)
-#email="septmacbot@gmail.com"
+# Need to create .git config by moein
+name=$(hostname)
+email="septmacbot@gmail.com"
 
-#GITCONFIG_FILE="$HOME/.gitconfig"
+GITCONFIG_FILE="$HOME/.gitconfig"
 
 # Create or overwrite .gitconfig
-#echo "[user]" > "$GITCONFIG_FILE"
-#echo "    name = $COMPUTER_NAME" >> "$GITCONFIG_FILE"
-#echo "    email = $EMAIL" >> "$GITCONFIG_FILE"
+echo "[user]" > "$GITCONFIG_FILE"
+echo "    name = $COMPUTER_NAME" >> "$GITCONFIG_FILE"
+echo "    email = $EMAIL" >> "$GITCONFIG_FILE"
 # Display success message
-# echo "Git configuration set successfully."
+echo "Git configuration set successfully."
 
 # this is for pulling
 cd ~/macbot
