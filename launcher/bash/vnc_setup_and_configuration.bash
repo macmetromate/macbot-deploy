20834054e0f4e135849628b97cfe66143a40c3c8
septmacbot <129236685+septmacbot@users.noreply.github.com>
2024-02-12 13:49:01 -0500

    Update vnc_setup_and_configuration.bash

--- launcher/bash/vnc_setup_and_configuration.bash
+++ launcher/bash/vnc_setup_and_configuration.bash
@@ -9,6 +9,7 @@ sudo touch ~/.vnc/xstartup
 sudo echo "unset SESSION_MANAGER" > ~/.vnc/xstartup
 sudo echo "unset DBUS_SESSION_BUS_ADDRESS" >> ~/.vnc/xstartup
 sudo echo "startlxde &" >> ~/.vnc/xstartup
+sudo cp ~/launcher/bash/xstartup ~/.vnc
 
 
 
