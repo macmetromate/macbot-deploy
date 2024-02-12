48b9cd73c5f9024d2804d1851f45c68001974c36
septmacbot <129236685+septmacbot@users.noreply.github.com>
2024-02-12 13:27:51 -0500

    Update vnc_setup_and_configuration.bash

--- launcher/bash/vnc_setup_and_configuration.bash
+++ launcher/bash/vnc_setup_and_configuration.bash
@@ -16,7 +16,7 @@ echo "PLEASE SET YOUR VNC ACCESS CREDENTIALS"
 # Will prompt for credentials
 sudo vncserver
 # Quietly checking and launching the vnc server upon ssh connection
-sudo echo "vncserver :2 -depth 24 -geometry 1920x1080 > /dev/null" >> ~/.bashrc
+sudo echo "sudo vncserver :2 -depth 24 -geometry 1920x1080 > /dev/null" >> ~/.bashrc
 
 # sudo wget --output-document=/usr/share/backgrounds/MacBotWallpaper.png https://github.com/septmacbot/macbot/raw/main/docs/Labs/Wallpapers/MARCWallpaper.png
 #sudo gsettings get org.gnome.desktop.background picture-uri 'file: ~/macbot/docs/Labs/Wallpapers/MARCWallpaper.png
