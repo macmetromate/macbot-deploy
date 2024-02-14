5281afa9c08ed395802371a006c587932d81063a
syrhassan10 <mahdih2@mcmaster.ca>
2024-02-14 14:01:18 -0500

    new ros melodic curl key

--- ros/ros_melodic_install.bash
+++ ros/ros_melodic_install.bash
@@ -1,15 +1,10 @@
-#!/bin/bash
-
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt install curl
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
-sudo apt -y update
+sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
+sudo apt-get update
 sudo apt install ros-melodic-desktop-full
-echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc
-source ~/.bashrc
-sudo apt -y install python-rosdep python-rosinstall python-rosinstall-generator python-wstool build-essential
+echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc #Add environment variables
+source ~/.bashrc #Effective environment variables
+sudo apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential #Install dependencies
 sudo apt install python-rosdep
 sudo rosdep init
 rosdep update
-
-
