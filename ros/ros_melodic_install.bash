efa6ad3af85008efc6933468739ea4207ab35e18
septmacbot <129236685+septmacbot@users.noreply.github.com>
2024-02-15 13:17:38 -0500

    Update ros_melodic_install.bash
    Add Curl and valid license

--- ros/ros_melodic_install.bash
+++ ros/ros_melodic_install.bash
@@ -1,7 +1,11 @@
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
 sudo apt-key adv --keyserver 'hkp://keyserver.ubuntu.com:80' --recv-key C1CF6E31E6BADE8868B172B4F42ED6FBAB17C654
 sudo apt-get update
+sudo apt install curl
+curl -sSL 'http://keyserver.ubuntu.com/pks/lookup?op=get&search=0xC1CF6E31E6BADE8868B172B4F42ED6FBAB17C654' | sudo apt-key add -
+sudo apt-get update
 sudo apt install ros-melodic-desktop-full
+
 echo "source /opt/ros/melodic/setup.bash" >> ~/.bashrc #Add environment variables
 source ~/.bashrc #Effective environment variables
 sudo apt-get install python-rosinstall python-rosinstall-generator python-wstool build-essential #Install dependencies
