f7c29d728391c46c5ae14316f129d64ea624aed1
septmacbot <129236685+septmacbot@users.noreply.github.com>
2024-02-15 13:16:22 -0500

    Update vnc_setup_and_configuration.bash
    Updated VNC, it will be on 5902

--- launcher/bash/vnc_setup_and_configuration.bash
+++ launcher/bash/vnc_setup_and_configuration.bash
@@ -3,6 +3,7 @@
 
 sudo apt install lxde
 sudo apt install tigervnc-standalone-server tigervnc-xorg-extension
+sudo apt-get install neofetch
 
 # Sometimes it fails to create this file, this is needed to use lxde within vnc
 mkdir ~/.vnc
@@ -10,15 +11,16 @@ touch ~/.vnc/xstartup
 echo "unset SESSION_MANAGER" > ~/.vnc/xstartup
 echo "unset DBUS_SESSION_BUS_ADDRESS" >> ~/.vnc/xstartup
 echo "startlxde &" >> ~/.vnc/xstartup
+sudo chmod +x ~/.vnc/xstartup
 #sudo cp ~/macbot/launcher/bash/xstartup ~/.vnc
 
 
 
 echo "PLEASE SET YOUR VNC ACCESS CREDENTIALS"
 # Will prompt for credentials
-sudo vncserver
+vncserver
 # Quietly checking and launching the vnc server upon ssh connection
-sudo echo "sudo vncserver :2 -depth 24 -geometry 1920x1080 > /dev/null" >> ~/.bashrc
+sudo echo "vncserver :2 -depth 24 -geometry 1920x1080 > /dev/null" >> ~/.bashrc
 
 # sudo wget --output-document=/usr/share/backgrounds/MacBotWallpaper.png https://github.com/septmacbot/macbot/raw/main/docs/Labs/Wallpapers/MARCWallpaper.png
 #sudo gsettings get org.gnome.desktop.background picture-uri 'file: ~/macbot/docs/Labs/Wallpapers/MARCWallpaper.png
